77bb1fb9116ff068fb4b536148b55f0502dc3e33
alviennn <alvien.rnp@gmail.com>
2023-06-30 00:41:34 +0700

    Loop whiledigunakan untuk mencari elemen dan akan terus berjalan selama currentNode tidak NULL dan nilai info pada currentNode tidak sama dengan elemen

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -50,6 +50,9 @@ public:
 	void search(string element, Node*& parent, Node*& currentNode){ //This function searches the cuurentNode of the specified Node current Node of its parent
 		currentNode = ROOT;
 		parent = NULL;
-		
+		while ((currentNode != NULL) && (currentNode->info != element)){
+			parent = currentNode;
+			
+		}
 	}
 };
